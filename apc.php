9db9ef17580e21b76eb2d7af39492b7f82efbdf5
krakjoe <joe.watkins@live.co.uk>
2013-03-01 16:43:26 +0000

    fix apc.php

--- apc.php
+++ apc.php
@@ -804,7 +804,7 @@ EOB;
 EOB;
 
 	$j = 0;
-	foreach (ini_get_all('apc') as $k => $v) {
+	foreach (ini_get_all('apcu') as $k => $v) {
 		echo "<tr class=tr-$j><td class=td-0>",$k,"</td><td>",str_replace(',',',<br />',$v['local_value']),"</td></tr>\n";
 		$j = 1 - $j;
 	}
