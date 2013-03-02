67178ac18b6ff4cf90557060ef6dc785d5a5ce10
krakjoe <joe.watkins@live.co.uk>
2013-03-02 08:36:42 +0000

    update version

--- php_apc.h
+++ php_apc.h
@@ -35,7 +35,7 @@
 #include "apc_php.h"
 #include "apc_globals.h"
 
-#define PHP_APC_VERSION "3.1.15-dev"
+#define PHP_APC_VERSION "4.0.0-dev"
 
 extern zend_module_entry apcu_module_entry;
 #define apcu_module_ptr &apcu_module_entry
