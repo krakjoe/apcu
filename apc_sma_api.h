1fa900488b81c48403b389fbc7c8ccf6397688b7
krakjoe <joe.watkins@live.co.uk>
2013-03-06 18:01:36 +0000

    copy

--- apc_sma_api.h
+++ apc_sma_api.h
@@ -1,3 +1,25 @@
+/*
+  +----------------------------------------------------------------------+
+  | APCu                                                                 |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2013 The PHP Group                                     |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Joe Watkins <joe.watkins@live.co.uk>                         |
+  +----------------------------------------------------------------------+
+ */
+
+/*
+ This module defines an SMA API for third parties
+*/
+
 #ifndef HAVE_APC_SMA_API_H
 #define HAVE_APC_SMA_API_H
 /* {{{ struct definition: apc_segment_t */
