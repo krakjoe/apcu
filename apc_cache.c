85677ff238623dbec2c4705d7cf8f5103f871fbb
Joe Watkins <krakjoe@php.net>
2015-08-26 21:14:24 +0100

    moar hash stuff

--- apc_cache.c
+++ apc_cache.c
@@ -1230,6 +1230,34 @@ static zend_always_inline void apc_array_dup_packed_elements(apc_context_t *ctxt
 	} while (p != end);
 }
 
+static zend_always_inline uint32_t apc_array_dup_elements(apc_context_t *ctxt, HashTable *source, HashTable *target, int static_keys, int with_holes)
+{
+    uint32_t idx = 0;
+	Bucket *p = source->arData;
+	Bucket *q = target->arData;
+	Bucket *end = p + source->nNumUsed;
+
+	do {
+		if (!apc_array_dup_element(ctxt, source, target, idx, p, q, 0, static_keys, with_holes)) {
+			uint32_t target_idx = idx;
+
+			idx++; p++;
+			while (p != end) {
+				if (apc_array_dup_element(ctxt, source, target, target_idx, p, q, 0, static_keys, with_holes)) {
+					if (source->nInternalPointer == idx) {
+						target->nInternalPointer = target_idx;
+					}
+					target_idx++; q++;
+				}
+				idx++; p++;
+			}
+			return target_idx;
+		}
+		idx++; p++; q++;
+	} while (p != end);
+	return idx;
+}
+
 static APC_HOTSPOT HashTable* my_copy_hashtable(HashTable *source, apc_context_t *ctxt) {
 	uint32_t idx;
 	HashTable *target;
