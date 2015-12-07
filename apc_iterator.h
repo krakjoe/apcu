de0ed5920f70fcd8734e04267e1ce66e5cad268b
Joe Watkins <krakjoe@php.net>
2015-12-07 08:56:18 +0000

    extern apc_iterator_delete

--- apc_iterator.h
+++ apc_iterator.h
@@ -98,6 +98,8 @@ PHP_APCU_API void apc_iterator_obj_init(
 	zend_long list);
 PHP_APCU_API zend_class_entry* apc_iterator_get_ce(void);
 PHP_APCU_API int apc_iterator_init(int module_number);
+
+extern int apc_iterator_delete(zval *key);
 #endif
 
 /*
