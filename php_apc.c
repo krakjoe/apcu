674f548fa957067e89d6f43a11cedeba3312700e
Joe Watkins <krakjoe@php.net>
2016-11-23 10:48:16 +0000

    try runtime stage so that value is not persistently allocated #4d9e95349d92b6cde745c9bd4bcaf66fbd014082

--- php_apc.c
+++ php_apc.c
@@ -225,7 +225,7 @@ static PHP_MINIT_FUNCTION(apcu)
 		/* disable cache gracefully using INI */
 		zend_string *ini = zend_string_init(
 			"apc.enabled", sizeof("apc.enabled")-1, 0);
-		zend_alter_ini_entry_chars(ini, "0", sizeof("0")-1, PHP_INI_SYSTEM, PHP_INI_STAGE_STARTUP);
+		zend_alter_ini_entry_chars(ini, "0", sizeof("0")-1, PHP_INI_SYSTEM, PHP_INI_STAGE_RUNTIME);
 		zend_string_release(ini);
     }
 
