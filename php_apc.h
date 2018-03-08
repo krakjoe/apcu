91fab866e7c641f3a6be729b62f759cd09b02c54
Remi Collet <remi@remirepo.net>
2018-03-08 06:16:26 +0100

    release 5.1.11

--- php_apc.h
+++ php_apc.h
@@ -33,7 +33,7 @@
 #include "apc.h"
 #include "apc_globals.h"
 
-#define PHP_APCU_VERSION "5.1.11-dev"
+#define PHP_APCU_VERSION "5.1.11"
 #define PHP_APCU_EXTNAME "apcu"
 
 PHP_APCU_API PHP_FUNCTION(apcu_add);
