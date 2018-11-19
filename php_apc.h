6443884c704ef29b5433018ea9a40dc35d82fb2c
Remi Collet <remi@remirepo.net>
2018-11-19 10:20:21 +0100

    back to dev

--- php_apc.h
+++ php_apc.h
@@ -33,7 +33,7 @@
 #include "apc.h"
 #include "apc_globals.h"
 
-#define PHP_APCU_VERSION "5.1.13"
+#define PHP_APCU_VERSION "5.1.14-dev"
 #define PHP_APCU_EXTNAME "apcu"
 
 PHP_APCU_API PHP_FUNCTION(apcu_add);
