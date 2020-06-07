6c36ebc3e59279dc7290ad894b6c639e6839a860
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 00:52:04 +0200

    PowerShell-ify
    * use string interpolation for better readability
    * replace aliases with their Cmdlets to avoid potential issues
    * use Cmdlets instead of the 7z command for better error handling
    * use Add-Content instead of output redirection
    * suppress useless output

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,28 +1,27 @@
 $ts_part = ''
-if ('0' -eq $env:TS) { $ts_part = '-nts' }
-$bname = 'php-devel-pack-' + $env:PHP_VER + $ts_part + '-Win32-' + $env:VC.toUpper() + '-' + $env:ARCH + '.zip'
-if (-not (Test-Path c:\build-cache\$bname)) {
-        Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
-        if (-not (Test-Path c:\build-cache\$bname)) {
-                Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
-        }
+if ($env:TS -eq '0') {
+    $ts_part += '-nts'
 }
-$dname0 = 'php-' + $env:PHP_VER + '-devel-' + $env:VC.toUpper() + '-' + $env:ARCH
-$dname1 = 'php-' + $env:PHP_VER + $ts_part + '-devel-' + $env:VC.toUpper() + '-' + $env:ARCH
-if (-not (Test-Path c:\build-cache\$dname1)) {
-        7z x c:\build-cache\$bname -oc:\build-cache
-        move c:\build-cache\$dname0 c:\build-cache\$dname1
+$bname = "php-devel-pack-$env:PHP_VER$ts_part-Win32-$env:VC-$env:ARCH.zip"
+if (-not (Test-Path "c:\build-cache\$bname")) {
+    Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
+    if (-not (Test-Path "c:\build-cache\$bname")) {
+        Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
+    }
 }
-cd c:\projects\apcu
-$env:PATH = 'c:\build-cache\' + $dname1 + ';' + $env:PATH
-#echo "@echo off" | Out-File -Encoding "ASCII" task.bat
-#echo "" | Out-File -Encoding "ASCII" -Append task.bat
-echo "" | Out-File -Encoding "ASCII" task.bat
-echo "call phpize 2>&1" | Out-File -Encoding "ASCII" -Append task.bat
-echo "call configure --enable-apcu --enable-debug-pack 2>&1" | Out-File -Encoding "ASCII" -Append task.bat
-echo "nmake /nologo 2>&1" | Out-File -Encoding "ASCII" -Append task.bat
-echo "exit %errorlevel%" | Out-File -Encoding "ASCII" -Append task.bat
-$here = (Get-Item -Path "." -Verbose).FullName
-$runner = 'c:\build-cache\php-sdk-' + $env:BIN_SDK_VER + '\phpsdk' + '-' + $env:VC + '-' + $env:ARCH + '.bat'
-$task = $here + '\task.bat'
-& $runner -t $task
+$dname0 = "php-$env:PHP_VER-devel-$env:VC-$env:ARCH"
+$dname1 = "php-$env:PHP_VER$ts_part-devel-$env:VC-$env:ARCH"
+if (-not (Test-Path "c:\build-cache\$dname1")) {
+    Expand-Archive "c:\build-cache\$bname" "c:\build-cache"
+    Move-Item "c:\build-cache\$dname0" "c:\build-cache\$dname1"
+}
+
+Set-Location 'c:\projects\apcu'
+$env:PATH = "c:\build-cache\$dname1;$env:PATH"
+
+$task = New-Item 'task.bat' -Force
+Add-Content $task "call phpize 2>&1"
+Add-Content $task "call configure --enable-apcu --enable-debug-pack 2>&1"
+Add-Content $task "nmake /nologo 2>&1"
+Add-Content $task "exit %errorlevel%"
+& "c:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,24 +1,22 @@
 $ts_part = ''
-if ('0' -eq $env:TS) { $ts_part = '-nts' }
-$bname = 'php-' + $env:PHP_VER + $ts_part + '-Win32-' + $env:VC.toUpper() + '-' + $env:ARCH + '.zip'
-if (-not (Test-Path c:\build-cache\$bname)) {
-        Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
-        if (-not (Test-Path c:\build-cache\$bname)) {
-                Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
-        }
+if ($env:TS -eq '0') {
+    $ts_part += '-nts'
 }
-$dname = 'php-' + $env:PHP_VER + $ts_part + '-' + $env:VC.toUpper() + '-' + $env:ARCH
-if (-not (Test-Path c:\build-cache\$dname)) {
-        7z x c:\build-cache\$bname -oc:\build-cache\$dname
+$bname = "php-$env:PHP_VER$ts_part-Win32-$env:VC-$env:ARCH.zip"
+if (-not (Test-Path "c:\build-cache\$bname")) {
+    Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
+    if (-not (Test-Path "c:\build-cache\$bname")) {
+        Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
+    }
 }
-cd c:\projects\apcu
-echo "" | Out-File -Encoding "ASCII" task.bat
-echo "set REPORT_EXIT_STATUS=1" | Out-File -Encoding "ASCII" -Append task.bat
-$cmd = 'call configure --enable-apcu --with-prefix=c:\build-cache\' + $dname + " 2>&1"
-echo $cmd | Out-File -Encoding "ASCII" -Append task.bat
-echo "nmake /nologo test TESTS=-q 2>&1" | Out-File -Encoding "ASCII" -Append task.bat
-echo "exit %errorlevel%" | Out-File -Encoding "ASCII" -Append task.bat
-$here = (Get-Item -Path "." -Verbose).FullName
-$runner = 'c:\build-cache\php-sdk-' + $env:BIN_SDK_VER + '\phpsdk' + '-' + $env:VC + '-' + $env:ARCH + '.bat'
-$task = $here + '\task.bat'
-& $runner -t $task
+$dname = "php-$env:PHP_VER$ts_part-$env:VC-$env:ARCH"
+if (-not (Test-Path "c:\build-cache\$dname")) {
+    Expand-Archive "c:\build-cache\$bname" "c:\build-cache\$dname"
+}
+Set-Location 'c:\projects\apcu'
+$task = New-Item 'task.bat' -Force
+Add-Content $task "set REPORT_EXIT_STATUS=1"
+Add-Content $task "call configure --enable-apcu --with-prefix=c:\build-cache\$dname 2>&1"
+Add-Content $task "nmake /nologo test TESTS=-q 2>&1"
+Add-Content $task "exit %errorlevel%"
+& "c:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
