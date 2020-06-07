bb8233ec6a2f710578df2b5ba1325ea5a8a84535
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 00:52:04 +0200

    PowerShell-ify
    * use string interpolation for better readability
    * replace aliases with their Cmdlets to avoid potential issues
    * use Cmdlets instead of the 7z command for better error handling
    * use Add-Content instead of output redirection
    * suppress useless output

--- .appveyor/package.ps1
+++ .appveyor/package.ps1
@@ -1,9 +1,15 @@
 $ts_part = 'ts'
-if ('0' -eq $env:TS) { $ts_part = 'nts' }
-$zip_bname = 'php_apcu-' + $env:APPVEYOR_REPO_COMMIT.substring(0, 8) + '-' + $env:PHP_VER.substring(0, 3) + '-' + $ts_part + '-' + $env:VC + '-' + $env:ARCH + '.zip'
-$dir = 'c:\projects\apcu\';
-if ('x64' -eq $env:ARCH) { $dir = $dir + 'x64\' }
-$dir = $dir + 'Release'
-if ('1' -eq $env:TS) { $dir = $dir + '_TS' }
-& 7z a c:\$zip_bname $dir\php_apcu.dll $dir\php_apcu.pdb c:\projects\apcu\LICENSE
-Push-AppveyorArtifact c:\$zip_bname
+if ($env:TS -eq '0') {
+    $ts_part += 'nts'
+}
+$zip_bname = 'php_apcu-' + $env:APPVEYOR_REPO_COMMIT.substring(0, 8) + '-' + $env:PHP_VER.substring(0, 3) + "-$ts_part-$env:VC-$env:ARCH.zip"
+$dir = 'c:\projects\apcu\'
+if ($env:ARCH -eq 'x64') {
+    $dir += 'x64\'
+}
+$dir += 'Release'
+if ($env:TS -eq '1') {
+    $dir += '_TS'
+}
+Compress-Archive @("$dir\php_apcu.dll", "$dir\php_apcu.pdb", "c:\projects\apcu\LICENSE") "c:\$zip_bname"
+Push-AppveyorArtifact "c:\$zip_bname"
