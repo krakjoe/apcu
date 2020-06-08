8b0a63f485b1787a6169a514072b8f93c1502d96
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 11:06:07 +0200

    Update PHP versions to build
    We always use latest PHP revision now.
    
    On PHP 7.4, some tests fail due to spurious output of PHP's built-in
    Webserver, which writes some output to stderr which is not caught in
    server_test.inc, because the `suppress_output` option of `proc_open()`
    does not suppress/ignore stderr, but merely ensures that no Windows
    dialog boxes for severe errors are shown, what could cause hangs.  We
    solve that by redirecting the childs stderr to a pipe.

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -15,20 +15,20 @@ if (-not (Test-Path 'c:\build-cache\$dname1')) {
     Move-Item "c:\build-cache\$dname0" "c:\build-cache\$dname1"
 }
 
+$releases = Invoke-WebRequest https://windows.php.net/downloads/releases/releases.json | ConvertFrom-Json
+$phpversion = $releases.$env:PHP_VER.version
+
 $ts_part = ''
 if ($env:TS -eq '0') {
     $ts_part += '-nts'
 }
 
-$bname = "php-devel-pack-$env:PHP_VER$ts_part-Win32-$env:VC-$env:ARCH.zip"
+$bname = "php-devel-pack-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
 if (-not (Test-Path "c:\build-cache\$bname")) {
-    Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
-    if (-not (Test-Path "c:\build-cache\$bname")) {
-        Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
-    }
+    Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
 }
-$dname0 = "php-$env:PHP_VER-devel-$env:VC-$env:ARCH"
-$dname1 = "php-$env:PHP_VER$ts_part-devel-$env:VC-$env:ARCH"
+$dname0 = "php-$phpversion-devel-$env:VC-$env:ARCH"
+$dname1 = "php-$phpversion$ts_part-devel-$env:VC-$env:ARCH"
 if (-not (Test-Path "c:\build-cache\$dname1")) {
     Expand-Archive "c:\build-cache\$bname" "c:\build-cache"
     if ($dname0 -ne $dname1) {
@@ -37,14 +37,11 @@ if (-not (Test-Path "c:\build-cache\$dname1")) {
 }
 $env:PATH = "c:\build-cache\$dname1;$env:PATH"
 
-$bname = "php-$env:PHP_VER$ts_part-Win32-$env:VC-$env:ARCH.zip"
+$bname = "php-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
 if (-not (Test-Path "c:\build-cache\$bname")) {
-    Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
-    if (-not (Test-Path "c:\build-cache\$bname")) {
-        Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
-    }
+    Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
 }
-$dname = "php-$env:PHP_VER$ts_part-$env:VC-$env:ARCH"
+$dname = "php-$phpversion$ts_part-$env:VC-$env:ARCH"
 if (-not (Test-Path "c:\build-cache\$dname")) {
     Expand-Archive "c:\build-cache\$bname" "c:\build-cache\$dname"
 }
