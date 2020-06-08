3fba5eb0c0f82f70c72078d5a19c6cd128905d72
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 14:29:43 +0200

    Add minimal build jobs for old PHP versions still supported by apcu
    We hard-code the last revisions of these versions.
    
    We also update the PHP-SDK to latest, and update the URL of the PHP-SDK
    repository.

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -5,8 +5,9 @@ if (-not (Test-Path 'c:\build-cache')) {
 }
 
 $bname = "php-sdk-$env:BIN_SDK_VER.zip"
+Write-Host $bname
 if (-not (Test-Path c:\build-cache\$bname)) {
-    Invoke-WebRequest "https://github.com/OSTC/php-sdk-binary-tools/archive/$bname" -OutFile "c:\build-cache\$bname"
+    Invoke-WebRequest "https://github.com/microsoft/php-sdk-binary-tools/archive/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname0 = "php-sdk-binary-tools-php-sdk-$env:BIN_SDK_VER"
 $dname1 = "php-sdk-$env:BIN_SDK_VER"
@@ -15,8 +16,18 @@ if (-not (Test-Path 'c:\build-cache\$dname1')) {
     Move-Item "c:\build-cache\$dname0" "c:\build-cache\$dname1"
 }
 
-$releases = Invoke-WebRequest https://windows.php.net/downloads/releases/releases.json | ConvertFrom-Json
-$phpversion = $releases.$env:PHP_VER.version
+$releases = @{
+    '7.0' = '7.0.33';
+    '7.1' = '7.1.33';
+}
+if ($releases.ContainsKey($env:PHP_VER)) {
+    $phpversion = $releases.$env:PHP_VER;
+    $base_url = 'http://windows.php.net/downloads/releases/archives';
+} else {
+    $releases = Invoke-WebRequest https://windows.php.net/downloads/releases/releases.json | ConvertFrom-Json
+    $phpversion = $releases.$env:PHP_VER.version
+    $base_url = 'http://windows.php.net/downloads/releases';
+}
 
 $ts_part = ''
 if ($env:TS -eq '0') {
@@ -25,7 +36,7 @@ if ($env:TS -eq '0') {
 
 $bname = "php-devel-pack-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
 if (-not (Test-Path "c:\build-cache\$bname")) {
-    Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
+    Invoke-WebRequest "$base_url/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname0 = "php-$phpversion-devel-$env:VC-$env:ARCH"
 $dname1 = "php-$phpversion$ts_part-devel-$env:VC-$env:ARCH"
@@ -39,7 +50,7 @@ $env:PATH = "c:\build-cache\$dname1;$env:PATH"
 
 $bname = "php-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
 if (-not (Test-Path "c:\build-cache\$bname")) {
-    Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
+    Invoke-WebRequest "$base_url/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname = "php-$phpversion$ts_part-$env:VC-$env:ARCH"
 if (-not (Test-Path "c:\build-cache\$dname")) {
