668ceb4aa40765a315b99d394bed0633d50e4fad
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 09:23:37 +0200

    Move installation steps to install.ps1
    We also execute run-tests.php manually, instead of doing the "task"
    dance.

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,27 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-$ts_part = ''
-if ($env:TS -eq '0') {
-    $ts_part += '-nts'
-}
-$bname = "php-$env:PHP_VER$ts_part-Win32-$env:VC-$env:ARCH.zip"
-if (-not (Test-Path "c:\build-cache\$bname")) {
-    Invoke-WebRequest "http://windows.php.net/downloads/releases/archives/$bname" -OutFile "c:\build-cache\$bname"
-    if (-not (Test-Path "c:\build-cache\$bname")) {
-        Invoke-WebRequest "http://windows.php.net/downloads/releases/$bname" -OutFile "c:\build-cache\$bname"
-    }
-}
-$dname = "php-$env:PHP_VER$ts_part-$env:VC-$env:ARCH"
-if (-not (Test-Path "c:\build-cache\$dname")) {
-    Expand-Archive "c:\build-cache\$bname" "c:\build-cache\$dname"
-}
 Set-Location 'c:\projects\apcu'
-$task = New-Item 'task.bat' -Force
-Add-Content $task "set REPORT_EXIT_STATUS=1"
-Add-Content $task "call configure --enable-apcu --with-prefix=c:\build-cache\$dname 2>&1"
-Add-Content $task "nmake /nologo test TESTS=-q 2>&1"
-Add-Content $task "exit %errorlevel%"
-& "c:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
+
+$env:TEST_PHP_EXECUTABLE = Get-Command 'php' | Select-Object -ExpandProperty 'Definition'
+& $env:TEST_PHP_EXECUTABLE 'run-tests.php' --show-diff tests
 if (-not $?) {
     throw "tests failed with errorlevel $LastExitCode"
 }
