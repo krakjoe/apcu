11f158c7153e801938197085fd9c831993e3e1c9
Christoph M. Becker <cmbecker69@gmx.de>
2020-06-08 11:06:07 +0200

    Update PHP versions to build
    We always use latest PHP revision now.
    
    On PHP 7.4, some tests fail due to spurious output of PHP's built-in
    Webserver, which writes some output to stderr which is not caught in
    server_test.inc, because the `suppress_output` option of `proc_open()`
    does not suppress/ignore stderr, but merely ensures that no Windows
    dialog boxes for severe errors are shown, what could cause hangs.  We
    solve that by redirecting the childs stderr to a pipe.

--- .appveyor/package.ps1
+++ .appveyor/package.ps1
@@ -4,7 +4,9 @@ $ts_part = 'ts'
 if ($env:TS -eq '0') {
     $ts_part += 'nts'
 }
-$zip_bname = 'php_apcu-' + $env:APPVEYOR_REPO_COMMIT.substring(0, 8) + '-' + $env:PHP_VER.substring(0, 3) + "-$ts_part-$env:VC-$env:ARCH.zip"
+$commit = $env:APPVEYOR_REPO_COMMIT.substring(0, 8)
+$zip_bname = "php_apcu-$commit-$env:PHP_VER-$ts_part-$env:VC-$env:ARCH.zip"
+
 $dir = 'c:\projects\apcu\'
 if ($env:ARCH -eq 'x64') {
     $dir += 'x64\'
@@ -13,5 +15,6 @@ $dir += 'Release'
 if ($env:TS -eq '1') {
     $dir += '_TS'
 }
+
 Compress-Archive @("$dir\php_apcu.dll", "$dir\php_apcu.pdb", "c:\projects\apcu\LICENSE") "c:\$zip_bname"
 Push-AppveyorArtifact "c:\$zip_bname"
