6d2410f2f4984387c66446fbd80f1af7ffde0665
Nikita Popov <nikita.ppv@gmail.com>
2020-07-20 16:44:57 +0200

    Restore compatibility with older PHP versions

--- php_apc.c
+++ php_apc.c
@@ -502,7 +502,9 @@ static void apc_store_helper(INTERNAL_FUNCTION_PARAMETERS, const zend_bool exclu
 /* {{{ proto bool apcu_enabled(void)
 	returns true when apcu is usable in the current environment */
 PHP_FUNCTION(apcu_enabled) {
-	ZEND_PARSE_PARAMETERS_NONE();
+	if (zend_parse_parameters_none() == FAILURE) {
+		return;
+	}
 	RETURN_BOOL(APCG(enabled));
 }
 /* }}} */
