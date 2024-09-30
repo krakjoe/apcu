635899cfe32496323d03e051e15be220f8b53d68
Nikita Popov <nikita.ppv@gmail.com>
2024-09-30 22:23:40 +0200

    Remove .appveyor directory

--- .appveyor/test.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-Set-Location 'c:\projects\apcu'
-
-$env:TEST_PHP_EXECUTABLE = Get-Command 'php' | Select-Object -ExpandProperty 'Definition'
-& $env:TEST_PHP_EXECUTABLE 'run-tests.php' --show-diff tests
-if (-not $?) {
-    throw "tests failed with errorlevel $LastExitCode"
-}
