krakjoe/apcu

commit 453a849b48912b9d3ea38e72fff9e8ebf30221fe

Make the key of a cache entry relocatable
The key is now implemented as the last part of apc_cache_entry_t, which
is a step toward making entries relocatable in the future. Furthermore,
this reduces the size of an entry by the size of a pointer and removes
one memcpy() of apc_cache_entry_t during insertion.

apc_cache.c
@@ -57,7 +57,7 @@
 
 /* Defined in apc_persist.c */
 apc_cache_entry_t *apc_persist(
-		apc_sma_t *sma, apc_serializer_t *serializer, const apc_cache_entry_t *orig_entry);
+		apc_sma_t *sma, apc_serializer_t *serializer, zend_string *key, const zval *val);
 zend_bool apc_unpersist(zval *dst, const zval *value, apc_serializer_t *serializer);
 
 /* {{{ make_prime */
@@ -135,9 +135,9 @@ static inline void apc_cache_hash_slot(
 } /* }}} */
 
 static inline zend_bool apc_entry_key_equals(const apc_cache_entry_t *entry, zend_string *key, zend_ulong hash) {
-	return ZSTR_H(entry->key) == hash
-		&& ZSTR_LEN(entry->key) == ZSTR_LEN(key)
-		&& memcmp(ZSTR_VAL(entry->key), ZSTR_VAL(key), ZSTR_LEN(key)) == 0;
+	return ZSTR_H(&entry->key) == hash
+		&& ZSTR_LEN(&entry->key) == ZSTR_LEN(key)
+		&& memcmp(ZSTR_VAL(&entry->key), ZSTR_VAL(key), ZSTR_LEN(key)) == 0;
 }
 
 /* An entry is hard expired if the creation time if older than the per-entry TTL.
@@ -213,7 +213,7 @@ static void apc_cache_wlocked_gc(apc_cache_t* cache)
 				if (dead->ref_count > 0) {
 					apc_debug(
 						"GC cache entry '%s' was on gc-list for %lld seconds",
-						ZSTR_VAL(dead->key), (long long) gc_sec
+						ZSTR_VAL(&dead->key), (long long) gc_sec
 					);
 				}
 
@@ -334,7 +334,7 @@ PHP_APCU_API apc_cache_t* apc_cache_create(apc_sma_t* sma, apc_serializer_t* ser
 
 static inline zend_bool apc_cache_wlocked_insert(
 		apc_cache_t *cache, apc_cache_entry_t *new_entry, zend_bool exclusive) {
-	zend_string *key = new_entry->key;
+	zend_string *key = &new_entry->key;
 	time_t t = new_entry->ctime;
 
 	/* process deleted list  */
@@ -391,26 +391,37 @@ static inline zend_bool apc_cache_wlocked_insert(
 	return 1;
 }
 
-static void apc_cache_init_entry(
-		apc_cache_entry_t *entry, zend_string *key, const zval* val, const int32_t ttl, time_t t);
+static void apc_cache_set_entry_values(apc_cache_entry_t *entry, const int32_t ttl, const time_t t)
+{
+	entry->ttl = ttl;
+	entry->next = NULL;
+	entry->ref_count = 0;
+	entry->mem_size = 0;
+	entry->nhits = 0;
+	entry->ctime = t;
+	entry->mtime = t;
+	entry->atime = t;
+	entry->dtime = 0;
+}
 
 /* TODO This function may lead to a deadlock on expunge */
 static inline zend_bool apc_cache_store_internal(
 		apc_cache_t *cache, zend_string *key, const zval *val,
 		const int32_t ttl, const zend_bool exclusive) {
-	apc_cache_entry_t tmp_entry, *entry;
 	time_t t = apc_time();
 
 	if (apc_cache_defense(cache, key, t)) {
 		return 0;
 	}
 
-	/* initialize the entry for insertion */
-	apc_cache_init_entry(&tmp_entry, key, val, ttl, t);
-	entry = apc_persist(cache->sma, cache->serializer, &tmp_entry);
+	/* create entry in the shared memory */
+	apc_cache_entry_t *entry = apc_persist(cache->sma, cache->serializer, key, val);
 	if (!entry) {
 		return 0;
 	}
+
+	/* init remaining values of the entry */
+	apc_cache_set_entry_values(entry, ttl, t);
 
 	/* execute an insertion */
 	if (!apc_cache_wlocked_insert(cache, entry, exclusive)) {
@@ -497,7 +508,6 @@ static inline apc_cache_entry_t *apc_cache_rlocked_find_incref(
 PHP_APCU_API zend_bool apc_cache_store(
 		apc_cache_t* cache, zend_string *key, const zval *val,
 		const int32_t ttl, const zend_bool exclusive) {
-	apc_cache_entry_t tmp_entry, *entry;
 	time_t t = apc_time();
 	zend_bool ret = 0;
 
@@ -510,12 +520,14 @@ PHP_APCU_API zend_bool apc_cache_store(
 		return 0;
 	}
 
-	/* initialize the entry for insertion */
-	apc_cache_init_entry(&tmp_entry, key, val, ttl, t);
-	entry = apc_persist(cache->sma, cache->serializer, &tmp_entry);
+	/* create entry in the shared memory */
+	apc_cache_entry_t *entry = apc_persist(cache->sma, cache->serializer, key, val);
 	if (!entry) {
 		return 0;
 	}
+
+	/* init remaining values of the entry */
+	apc_cache_set_entry_values(entry, ttl, t);
 
 	/* execute an insertion */
 	if (!apc_cache_wlock(cache)) {
@@ -978,25 +990,6 @@ PHP_APCU_API zend_bool apc_cache_entry_fetch_zval(
 }
 /* }}} */
 
-/* {{{ apc_cache_make_entry */
-static void apc_cache_init_entry(
-		apc_cache_entry_t *entry, zend_string *key, const zval *val, const int32_t ttl, time_t t)
-{
-	entry->ttl = ttl;
-	entry->key = key;
-	ZVAL_COPY_VALUE(&entry->val, val);
-
-	entry->next = NULL;
-	entry->ref_count = 0;
-	entry->mem_size = 0;
-	entry->nhits = 0;
-	entry->ctime = t;
-	entry->mtime = t;
-	entry->atime = t;
-	entry->dtime = 0;
-}
-/* }}} */
-
 static inline void array_add_long(zval *array, zend_string *key, zend_long lval) {
 	zval zv;
 	ZVAL_LONG(&zv, lval);
@@ -1015,7 +1008,7 @@ static zval apc_cache_link_info(apc_cache_t *cache, apc_cache_entry_t *p)
 	zval link, zv;
 	array_init(&link);
 
-	ZVAL_STR(&zv, zend_string_dup(p->key, 0));
+	ZVAL_STR(&zv, zend_string_dup(&p->key, 0));
 	zend_hash_add_new(Z_ARRVAL(link), apc_str_info, &zv);
 
 	array_add_long(&link, apc_str_ttl, p->ttl);

apc_cache.h
@@ -49,7 +49,6 @@ struct apc_cache_slam_key_t {
 /* {{{ struct definition: apc_cache_entry_t */
 typedef struct apc_cache_entry_t apc_cache_entry_t;
 struct apc_cache_entry_t {
-	zend_string *key;        /* entry key */
 	zval val;                /* the zval copied at store time */
 	apc_cache_entry_t *next; /* next entry in linked list */
 	zend_long ttl;           /* the ttl on this specific entry */
@@ -60,6 +59,7 @@ struct apc_cache_entry_t {
 	time_t dtime;            /* time entry was removed from cache */
 	time_t atime;            /* time entry was last accessed */
 	zend_long mem_size;      /* memory used */
+	zend_string key;         /* entry key (MUST BE THE LAST FIELD OF THE STRUCT!) */
 };
 /* }}} */
 
@@ -297,7 +297,8 @@ static inline void apc_cache_runlock(apc_cache_t *cache) {
 	}
 }
 
-#define APC_ENTRY_MIN_ALLOC_SIZE (ZEND_MM_ALIGNED_SIZE(sizeof(apc_cache_entry_t)) + ZEND_MM_ALIGNED_SIZE(_ZSTR_STRUCT_SIZE(1)))
+/* APC_ENTRY_SIZE takes into account the trailing key-string + terminating 0-byte */
+#define APC_ENTRY_SIZE(key_len) (ZEND_MM_ALIGNED_SIZE(XtOffsetOf(apc_cache_entry_t, key.val) + key_len + 1))
 
 #endif
 

apc_iterator.c
@@ -55,7 +55,7 @@ static apc_iterator_item_t* apc_iterator_item_ctor(
 	array_init(&item->value);
 	ht = Z_ARRVAL(item->value);
 
-	item->key = zend_string_dup(entry->key, 0);
+	item->key = zend_string_dup(&entry->key, 0);
 
 	if (APC_ITER_TYPE & iterator->format) {
 		ZVAL_STR_COPY(&zv, apc_str_user);
@@ -178,18 +178,18 @@ static int apc_iterator_search_match(apc_iterator_t *iterator, apc_cache_entry_t
 #if PHP_VERSION_ID >= 70300
 		rval = pcre2_match(
 			php_pcre_pce_re(iterator->pce),
-			(PCRE2_SPTR) ZSTR_VAL(entry->key), ZSTR_LEN(entry->key),
+			(PCRE2_SPTR) ZSTR_VAL(&entry->key), ZSTR_LEN(&entry->key),
 			0, 0, iterator->re_match_data, php_pcre_mctx()) >= 0;
 #else
 		rval = pcre_exec(
 			iterator->pce->re, iterator->pce->extra,
-			ZSTR_VAL(entry->key), ZSTR_LEN(entry->key),
+			ZSTR_VAL(&entry->key), ZSTR_LEN(&entry->key),
 			0, 0, NULL, 0) >= 0;
 #endif
 	}
 
 	if (iterator->search_hash) {
-		rval = zend_hash_exists(iterator->search_hash, entry->key);
+		rval = zend_hash_exists(iterator->search_hash, &entry->key);
 	}
 
 	return rval;

apc_persist.c
@@ -86,7 +86,7 @@ static inline void apc_persist_copy_zval(apc_persist_context_t *ctxt, zval *zv) 
 	apc_persist_copy_zval_impl(ctxt, zv);
 }
 
-void apc_persist_init_context(apc_persist_context_t *ctxt, apc_serializer_t *serializer) {
+static void apc_persist_init_context(apc_persist_context_t *ctxt, apc_serializer_t *serializer) {
 	ctxt->serializer = serializer;
 	ctxt->size = 0;
 	ctxt->memoization_needed = 0;
@@ -97,7 +97,7 @@ void apc_persist_init_context(apc_persist_context_t *ctxt, apc_serializer_t *ser
 	ctxt->alloc_cur = NULL;
 }
 
-void apc_persist_destroy_context(apc_persist_context_t *ctxt) {
+static void apc_persist_destroy_context(apc_persist_context_t *ctxt) {
 	if (ctxt->memoization_needed) {
 		zend_hash_destroy(&ctxt->already_counted);
 		zend_hash_destroy(&ctxt->already_allocated);
@@ -235,10 +235,10 @@ static zend_bool apc_persist_calc_zval(apc_persist_context_t *ctxt, const zval *
 	}
 }
 
-static zend_bool apc_persist_calc(apc_persist_context_t *ctxt, const apc_cache_entry_t *entry) {
-	ADD_SIZE(sizeof(apc_cache_entry_t));
-	ADD_SIZE_STR(ZSTR_LEN(entry->key));
-	return apc_persist_calc_zval(ctxt, &entry->val);
+static zend_bool apc_persist_calc(apc_persist_context_t *ctxt, const zend_string *key, const zval *zv) {
+	ADD_SIZE(APC_ENTRY_SIZE(ZSTR_LEN(key)));
+
+	return apc_persist_calc_zval(ctxt, zv);
 }
 
 static inline void *apc_persist_get_already_allocated(apc_persist_context_t *ctxt, void *ptr) {
@@ -435,27 +435,40 @@ static void apc_persist_copy_zval_impl(apc_persist_context_t *ctxt, zval *zv) {
 	}
 }
 
-static apc_cache_entry_t *apc_persist_copy(
-		apc_persist_context_t *ctxt, const apc_cache_entry_t *orig_entry) {
-	apc_cache_entry_t *entry = COPY(orig_entry, sizeof(apc_cache_entry_t));
-	entry->key = apc_persist_copy_zstr_no_add(ctxt, entry->key);
+static apc_cache_entry_t *apc_persist_create_entry(
+		apc_persist_context_t *ctxt, zend_string *key, const zval *zv)
+{
+	/* Get memory for the entry (incl. key) */
+	apc_cache_entry_t *entry = ALLOC(APC_ENTRY_SIZE(ZSTR_LEN(key)));
+
+	/* Deep copy of the key */
+	GC_SET_REFCOUNT(&entry->key, 1);
+	GC_SET_PERSISTENT_TYPE(&entry->key, IS_STRING);
+	ZSTR_LEN(&entry->key) = ZSTR_LEN(key);
+	memcpy(ZSTR_VAL(&entry->key), ZSTR_VAL(key), ZSTR_LEN(key));
+	ZSTR_VAL(&entry->key)[ZSTR_LEN(key)] = '\0';
+	ZSTR_H(&entry->key) = zend_string_hash_val(key);
+
+	/* Deep copy of the value */
+	ZVAL_COPY_VALUE(&entry->val, zv);
 	apc_persist_copy_zval(ctxt, &entry->val);
+
 	return entry;
 }
 
 apc_cache_entry_t *apc_persist(
-		apc_sma_t *sma, apc_serializer_t *serializer, const apc_cache_entry_t *orig_entry) {
+		apc_sma_t *sma, apc_serializer_t *serializer, zend_string *key, const zval *val) {
 	apc_persist_context_t ctxt;
 	apc_cache_entry_t *entry;
 
 	apc_persist_init_context(&ctxt, serializer);
 
 	/* The top-level value should never be a reference */
-	ZEND_ASSERT(Z_TYPE(orig_entry->val) != IS_REFERENCE);
+	ZEND_ASSERT(Z_TYPE_P(val) != IS_REFERENCE);
 
 	/* If we're serializing an array using the default serializer, we will have
 	 * to keep track of potentially repeated refcounted structures. */
-	if (!serializer && Z_TYPE(orig_entry->val) == IS_ARRAY) {
+	if (!serializer && Z_TYPE_P(val) == IS_ARRAY) {
 		ctxt.memoization_needed = 1;
 		zend_hash_init(&ctxt.already_counted, 0, NULL, NULL, 0);
 		zend_hash_init(&ctxt.already_allocated, 0, NULL, NULL, 0);
@@ -463,12 +476,12 @@ apc_cache_entry_t *apc_persist(
 
 	/* Objects are always serialized, and arrays when a serializer is set.
 	 * Other cases are detected during apc_persist_calc(). */
-	if (Z_TYPE(orig_entry->val) == IS_OBJECT
-			|| (serializer && Z_TYPE(orig_entry->val) == IS_ARRAY)) {
+	if (Z_TYPE_P(val) == IS_OBJECT
+			|| (serializer && Z_TYPE_P(val) == IS_ARRAY)) {
 		ctxt.use_serialization = 1;
 	}
 
-	if (!apc_persist_calc(&ctxt, orig_entry)) {
+	if (!apc_persist_calc(&ctxt, key, val)) {
 		if (!ctxt.use_serialization) {
 			apc_persist_destroy_context(&ctxt);
 			return NULL;
@@ -478,7 +491,7 @@ apc_cache_entry_t *apc_persist(
 		apc_persist_destroy_context(&ctxt);
 		apc_persist_init_context(&ctxt, serializer);
 		ctxt.use_serialization = 1;
-		if (!apc_persist_calc(&ctxt, orig_entry)) {
+		if (!apc_persist_calc(&ctxt, key, val)) {
 			apc_persist_destroy_context(&ctxt);
 			return NULL;
 		}
@@ -490,7 +503,7 @@ apc_cache_entry_t *apc_persist(
 		return NULL;
 	}
 
-	entry = apc_persist_copy(&ctxt, orig_entry);
+	entry = apc_persist_create_entry(&ctxt, key, val);
 	ZEND_ASSERT(ctxt.alloc_cur == ctxt.alloc + ctxt.size);
 
 	entry->mem_size = ctxt.size;

php_apc.c
@@ -231,7 +231,7 @@ static PHP_MINIT_FUNCTION(apcu)
 			/* initialize shared memory allocator */
 			apc_sma_init(
 				&apc_sma, (void **) &apc_user_cache, (apc_sma_expunge_f) apc_cache_default_expunge,
-				APCG(shm_size), APC_ENTRY_MIN_ALLOC_SIZE, mmap_file_mask);
+				APCG(shm_size), APC_ENTRY_SIZE(0), mmap_file_mask);
 
 			REGISTER_LONG_CONSTANT(APC_SERIALIZER_CONSTANT, (zend_long)&_apc_register_serializer, CONST_PERSISTENT | CONST_CS);
 

commit 1e5c46559e5ed56eebfbdc871c7d512af246eb43

Fix mem_size of cache entries
This fixes a bug introduced in 453a849b / #554. The size was erroneously
reset to 0 by apc_cache_set_entry_values() after it has been correctly
set by apc_persist().

apc_cache.c
@@ -396,7 +396,6 @@ static void apc_cache_set_entry_values(apc_cache_entry_t *entry, const int32_t t
 	entry->ttl = ttl;
 	entry->next = NULL;
 	entry->ref_count = 0;
-	entry->mem_size = 0;
 	entry->nhits = 0;
 	entry->ctime = t;
 	entry->mtime = t;