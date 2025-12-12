f0fd3837083445cf1527567c7a01b4c351423662
Arndt Kaiser <madmajestro@gmail.com>
2025-12-12 15:36:01 +0100

    Check files referenced by package.xml in the CI
    The script checkrel.sh is used in the new CI job "common" to check if a
    file is missing in package.xml. Is also has been improved to be
    compatible with the Dash shell and to return an unsuccessful exit code
    if the check fails.

--- checkrel.sh
+++ checkrel.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
+
+exit_code=0
+
 echo "+ check all files in package.xml"
 
 for i in $(ls *.c *.h)
 do
-	[ $i == config.h ] && continue
-	grep -q $i package.xml || echo missing $i
+	[ $i = config.h ] && continue
+	grep -q $i package.xml || {
+		echo "missing file $i in package.xml"
+		exit_code=1
+	}
 done
+
 for i in $(find tests -type f)
 do
-	j=$(basename $i)
-	[ $j == .gitignore ] && continue
-	grep -q $j package.xml || echo missing $j
+	i=$(basename $i)
+	[ $i = .gitignore ] && continue
+	grep -q $i package.xml || {
+		echo "missing file $i in package.xml"
+		exit_code=1
+	}
 done
+
+exit $exit_code
